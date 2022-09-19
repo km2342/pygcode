c24b2acd7847c30ea1ab6d92ece57591e1ce9dbf
Katie McCready <katiermccready@gmail.com>
2022-09-19 10:55:01 -0500

    setup.py file modifications

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import codecs
 import os
-import re
 from distutils.version import LooseVersion
 
 from setuptools import setup, find_packages
-
+import src.pygcode.__version__ as metadata
 
 # Setup template thanks to: Hynek Schlawack
 #   https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
@@ -12,7 +11,8 @@ from setuptools import setup, find_packages
 
 NAME = "pygcode"
 PACKAGES = find_packages(where="src")
-META_PATH = os.path.join("src", NAME, "__init__.py")
+HERE = os.path.abspath(os.path.dirname(__file__))
+
 KEYWORDS = ['gcode', 'cnc', 'parser', 'interpreter']
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",  # see src/pygcode/__init__.py
@@ -38,81 +38,28 @@ SCRIPTS = [
 
 ###################################################################
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
 
 def read(*parts):
     """
-    Build an absolute path from *parts* and and return the contents of the
+    Build an absolute path from *parts* and return the contents of the
     resulting file.  Assume UTF-8 encoding.
     """
     with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
         return f.read()
 
 
-META_FILE = read(META_PATH)
-
-
-def find_meta(meta):
-    """
-    Extract __*meta*__ from META_FILE.
-    """
-    meta_match = re.search(
-        r"^(?P<name>__{meta}__)\s*=\s*['\"](?P<value>[^'\"]*)['\"](\s*#.*)?$".format(meta=meta),
-        META_FILE, re.M
-    )
-    if meta_match:
-        return meta_match.group('value')
-    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
-
-
-def assert_version_classifier(version_str):
-    """
-    Verify version consistency:
-    version number must correspond to the correct "Development Status" classifier
-    :raises: ValueError if error found, but ideally this function does nothing
-    """
-    V = lambda v: LooseVersion(v)
-    # cast version
-    version = V(version_str)
-
-    # get "Development  Status" classifier
-    dev_status_list = [x for x in CLASSIFIERS if x.startswith("Development Status ::")]
-    if len(dev_status_list) != 1:
-        raise ValueError("must be 1 'Development Status' in CLASSIFIERS")
-    classifier = dev_status_list.pop()
-
-    version_map = [
-        (V('0.1'), "Development Status :: 2 - Pre-Alpha"),
-        (V('0.2'), "Development Status :: 3 - Alpha"),
-        (V('0.3'), "Development Status :: 4 - Beta"),
-        (V('1.0'), "Development Status :: 5 - Production/Stable"),
-    ]
-
-    for (test_ver, test_classifier) in reversed(sorted(version_map, key=lambda x: x[0])):
-        if version >= test_ver:
-            if classifier == test_classifier:
-                return  # all good, now forget any of this ever happened
-            else:
-                raise ValueError("for version {ver} classifier should be \n'{good}'\nnot\n'{bad}'".format(
-                    ver=str(version), good=test_classifier, bad=classifier
-                ))
-
-
 if __name__ == "__main__":
-#     version = find_meta("version")
-#     assert_version_classifier(version)
 
     setup(
         name=NAME,
-        description=find_meta("description"),
-        license=find_meta("license"),
-        url=find_meta("url"),
-        version="0.2.1",
-        author=find_meta("author"),
-        author_email=find_meta("email"),
-        maintainer=find_meta("author"),
-        maintainer_email=find_meta("email"),
+        description=metadata.__description__,
+        license=metadata.__license__,
+        url=metadata.__url__,
+        version=metadata.__version__,
+        author=metadata.__author__,
+        author_email=metadata.__email__,
+        maintainer=metadata.__author__,
+        maintainer_email=metadata.__email__,
         keywords=KEYWORDS,
         long_description=read("README.rst"),
         packages=PACKAGES,
